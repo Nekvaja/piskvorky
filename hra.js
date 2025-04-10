a49a4c7869b35643a38ff57620cd2869da80fda7
Jana Nekvapilová <jana.nekva@gmail.com>
2025-04-10 14:02:23 +0200

    přidáno potvrzení pro restart hry

--- hra.js
+++ hra.js
@@ -1,23 +1,39 @@
 let currentPlayer = 'circle';
 
 const buttonsElement = document.querySelectorAll("button");
+const naTahuElement = document.querySelector(".hrac__znak");
+const btnRestartElement = document.querySelector(".btn--restart")
 
 const tah = (event) => {
     
-
     if (currentPlayer === "circle") {
         event.target.classList.add("board__field--circle")
+        event.target.disabled = true
         currentPlayer = "cross"
+        naTahuElement.classList.remove("hrac__znak--circle")
+        naTahuElement.classList.add("hrac__znak--cross")
     } else {
         event.target.classList.add("board__field--cross")
+        event.target.disabled = true
         currentPlayer = "circle"
+        naTahuElement.classList.remove("hrac__znak--cross")
+        naTahuElement.classList.add("hrac__znak--circle")
     }
-
-
-    console.log(currentPlayer)
 }
 
 buttonsElement.forEach((tlacitko) => {
 tlacitko.addEventListener("click", tah)
 })
+
+const zacitZnovu = (event) => {
+    const potvrzeni = confirm("Opravdu chceš začít znovu?")
+    if (!potvrzeni) {
+        event.preventDefault();
+    }
+}
+
+
+btnRestartElement.addEventListener("click", zacitZnovu)
+
+
 
